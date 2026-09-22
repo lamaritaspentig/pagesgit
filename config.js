7ddf1fc2ba30b47d27d427f92fef7ab8482e5982
lamaritaspentig <smpn03mksr@gmail.com>
2026-09-22 17:08:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GITPAGE_CONFIG = {
   // Tempel URL Deployment Web App Google Apps Script yang berakhir dengan /exec
-  APPS_SCRIPT_URL: ''
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUYqywkII1LY1GFtiYknSpFo04obbpnjX_Jl1WmkfkZ81DX7yACCREBySbCPcsWQa4iQ/exec'
 };
